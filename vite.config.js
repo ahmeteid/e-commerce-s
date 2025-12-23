9d9931d4c8b321c918409ffc2a8c482f0e0f339d
ahmeteid <ahmeteid355@gmail.com>
2025-12-23 19:24:16 +0300

    ptoject done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit for Material UI (it's legitimately large)
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Separate vendor chunks for better caching
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui-vendor': [
+            '@mui/material',
+            '@mui/icons-material',
+            '@emotion/react',
+            '@emotion/styled',
+          ],
+          'api-vendor': ['axios'],
+        },
+      },
+    },
+  },
 })
 
